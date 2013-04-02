295bcb55014e59f897f4a5d48911a7793d0a4c72
Hackbright Student <user@mini-003.(none)>
2013-04-01 18:10:49 -0700

    Starting exercise, creating classes and starting to seed the database

--- model.py
+++ model.py
@@ -1,14 +1,62 @@
+import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+ENGINE = None
+Session = None
+
+
+Base = declarative_base() #This is required by SQLAlchemy 
 
 ### Class declarations go here
+class User(Base):
 
+	__tablename__ = "users" # tells SQLAlchemy  instance stored in table named users
+
+	id = Column(Integer, primary_key = True)
+	email = Column(String(64), nullable = True)
+	password = Column(String(64), nullable  = True)
+	age = Column(Integer, nullable = True)
+	zipcode = Column(String(15),nullable=True)
+
+	# apparently don't need this function, it's optional
+	# def __init__(self,  email = None, password = None, age = None, zipcode = None):
+	# 	self.email = email
+	# 	self.password = password
+	# 	self.age = age
+	# 	self.zipcode = zipcode
+
+class Movies(Base):
+
+	__tablename__ = "movies"
+
+	id = Column(Integer, primary_key = True)
+	movie_title = Column(String(128), nullable = False)
+	released_at = Column(DateTime(),  default=datetime.datetime.now)	
+	imdb_url = Column(String(128), nullable = True)
+
+class Ratings(Base):
+
+	__tablename__ = "ratings"
+
+	id = Column(Integer, primary_key = True)
+	movie_id = Column(Integer(15), nullable = False)
+	rating = Column(Integer(1), nullable = False)
+	released_at = Column(DateTime(),  default=datetime.datetime.now)
 
 ### End class declarations
 
+def connect():
+	global ENGINE
+	global Session
+
+	#This is SQLAlchemy's way of interacting with the db, creating a session
+	ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+	Session = sessionmaker(bind=ENGINE)
+
+	return Session()
 def main():
     """In case we need this for something"""
     pass
--- seed.py
+++ seed.py
@@ -1,8 +1,30 @@
 import model
 import csv
 
+
+
 def load_users(session):
     # use u.user
+
+    #open a file
+    with open('seed_data/u.user','rb') as csvfile:
+        userList = csv.reader(csvfile, delimiter=' ', quotechar='/')
+        #read a line
+        for row in userList:
+        #parse a line
+            dbRow = row[0].split('|')
+            print dbRow
+        #create an object
+            user = model.User(email = "email", password = "password", age = dbRow[1], zipcode=dbRow[4])
+        #add the object to a session
+            session.add(user)
+            session.commit() # do i commit after every iteration or can i commit at the end of the loop
+            break
+
+    #commit session
+
+    #repeat until done
+
     pass
 
 def load_movies(session):
@@ -15,7 +37,8 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    #pass
 
 if __name__ == "__main__":
     s= model.connect()
